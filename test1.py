21af0d8b6a15c3606740fa63c0fdd9662f0e6148
z <las.kzn@gmail.com>
2025-06-20 18:28:45 +0300

    .

--- test1.py
+++ test1.py
@@ -33,13 +33,6 @@ def pe():
 def logo_png():
     return send_file("images/logo.png")
 
-# @app.route("/login/", methods=["post", "get"])
-# def login_page():
-#     if request.method == "GET":
-#         return render_template("login.html")
-#     else:
-#         print(request)
-#         return "Text"
 @app.route('/login/', methods=['post', 'get'])
 def login():
     message = ''
